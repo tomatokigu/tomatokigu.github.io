93a73152642850fcac83a537b193f4e546b94c06
tomatokigu <tomatokigu@users.noreply.github.com>
2021-01-28 02:26:38 +0000

    jekyll build from Action e60d4098da8a923f24e51d30995ea2f9b80503d3

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".navbar-burger").on('click', function() {
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+  });
+});
